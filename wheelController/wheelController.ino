b15e748b02289de82fb78c8dcc2063881c14e054
David Irvine <irvined@gmail.com>
2013-03-05 23:18:22 -0400

    Beta Wheel Controller

--- wheelController/wheelController.ino
+++ wheelController/wheelController.ino
@@ -1,64 +1,58 @@
-#define OP_HORN 1
-#define OP_LIND 2
-#define OP_RIND 3
-#define OP_DIP 5
-#define OP_GRUP 6
-#define OP_GRDN 7
+#define IN_LIND 4 // Left Indicator Switch connected to pin 4
+#define IN_RIND 5 // Left Indicator Switch connected to pin 5
+#define IN_HORN 6 // Horn Switch connected to pin 6
+#define IN_DIP 7  // Dip Switch connected to pin 7
+#define IN_GRUP 8 // Gear Up Switch connected to pin 8
+#define IN_GRDN 9 // Gear Down Switch connected to pin 9
 
-#define IN_HORN 6
-#define IN_LIND 7
-#define IN_RIND 8
-#define IN_HAZ  9
-#define IN_DIP 10
-#define IN_GRUP 11
-#define IN_GRDN 12
 
+// Array indexes of states
 #define ST_HORN  0
 #define ST_LIND  1
-#define ST_RIND 2
-#define ST_HAZ  3
+#define ST_RIND 3
 #define ST_DIP 4
 #define ST_GRUP 5
 #define ST_GRDN 6
-
-#define DW digitalWrite
-#define DR digitalRead
-
-#define FLASHRATE 500
-
 bool featureStates[7];
 
-unsigned long flashCounter;
-bool flashing;
+#define DR digitalRead
+#define DW digitalWrite
+
+
 
 void setup(){
   // setup Serial communication
   Serial.begin(115200);
-  
-  flashing=true;
-  flashCounter=millis()+ FLASHRATE ;
-  digitalWrite(2, INPUT);
-  digitalWrite(3, INPUT);
+  // Set the interrupt pins to inputs
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  // Hold the interrupt pins high, they will then be 
+  // pulled low when the switches are activated and the 
+  // pins on the switch will be high when read.
+  DW(2,HIGH);
+  DW(3,HIGH);
+  int i;
+  // Set all pins to input
+  // Hold them low.
+  for (i=4;i<=9;++i){
+    pinMode(i,INPUT);
+    DW(1,LOW);
+  }
+  // Attatch an interrupt handler to the interrupt pins.  Right now
+  // This is only used to detect input from the indicator switches
+  // however multiple switches can use the same pin.
   attachInterrupt(0, buttonPress, CHANGE);
   attachInterrupt(1, buttonPress, CHANGE);
 }
 
 void loop(){
-  checkFlash();
+  // Check the state of pins that are on or off directly, as opposed 
+  // to those that toggle the values or have special actions
   getSimpleStates();
-  updateOutputs();
+  // send the states to the main controller
+  sendStates();
 }
 
-
-
-
-
-void checkFlash(){
-  if (millis()>flashCounter){
-    flashing=!flashing;
-    flashCounter=millis()+FLASHRATE;
-  }  
-}
 
 void getSimpleStates(){
  if ( DR(IN_HORN) ){
@@ -89,8 +83,7 @@ void getSimpleStates(){
 void buttonPress(){
  
   if ( DR(IN_LIND) ){
-   if (featureStates[ST_HAZ] || featureStates[ST_RIND] || featureStates[ST_LIND]){
-     featureStates[ST_HAZ]=false;
+   if (featureStates[ST_RIND] || featureStates[ST_LIND]){
      featureStates[ST_LIND]=false;
      featureStates[ST_RIND]=false;
    }else{
@@ -98,43 +91,20 @@ void buttonPress(){
    }
   }
   if ( DR(IN_RIND) ){
-    if (featureStates[ST_HAZ] || featureStates[ST_RIND] || featureStates[ST_LIND]){
-     featureStates[ST_HAZ]=false;
+    if (featureStates[ST_RIND] || featureStates[ST_LIND]){
      featureStates[ST_LIND]=false;
      featureStates[ST_RIND]=false;
    }else{
      featureStates[ST_RIND]=true;
    }
   }
-  if ( DR(IN_HAZ) ){
-    if (featureStates[ST_RIND] || featureStates[ST_LIND]){
-     featureStates[ST_HAZ]=true;
-     featureStates[ST_LIND]=false;
-     featureStates[ST_RIND]=false;
-   }else if(featureStates[ST_HAZ]){
-     featureStates[ST_HAZ]=false;
-   }else{
-     featureStates[ST_HAZ]=true;
-   }
-  }
 }
 
-void updateOutputs(){
-  DW(OP_HORN, featureStates[ST_HORN] ? HIGH:LOW);
-  DW(OP_DIP, featureStates[ST_DIP] ? HIGH:LOW);
-  DW(OP_GRUP, featureStates[ST_GRUP] ? HIGH:LOW);
-  DW(OP_GRDN, featureStates[ST_GRDN] ? HIGH:LOW);
-  
-  if (featureStates[ST_HAZ]||featureStates[ST_LIND]){
-    DW(OP_LIND,flashing ? HIGH : LOW);
-  }else{
-    DW(OP_LIND,LOW);
+void sendStates(){
+  unsigned int i;
+  for (i=0;i<sizeof(featureStates)/sizeof(featureStates[0]); ++i){
+    Serial.print(featureStates[i] ? 1:0);
   }
-  if (featureStates[ST_HAZ]||featureStates[ST_RIND]){
-    DW(OP_RIND,flashing ? HIGH : LOW);
-  }else{
-    DW(OP_RIND,LOW);
-  }
-
+  Serial.print("\n");
 }
 
